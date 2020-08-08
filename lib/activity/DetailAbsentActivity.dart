85fa1a2739b6aa7945e9ce7939d746ce75e45962
dendhi93 <44012078+dendhi93@users.noreply.github.com>
2020-08-08 21:25:09 +0700

    create text field

--- lib/activity/DetailAbsentActivity.dart
+++ lib/activity/DetailAbsentActivity.dart
@@ -6,9 +6,44 @@ import 'package:flutter/material.dart';
 class DetailAbsentActivity extends StatelessWidget {
 
   Widget _initDetail(){
-    return Container(
-      padding: EdgeInsets.all(12.0),
-      child: Text('Flat Button'),
+//    return Container(
+//      padding: EdgeInsets.all(12.0),
+//      child: Text('Flat Button'),
+//    );
+
+    return Material(
+        child: new Container(
+          padding: const EdgeInsets.all(10.0),
+          color: Colors.white,
+          child: new Container(
+            child: new Center(
+              child: new Column(
+                children: <Widget>[
+                      new Padding(padding: EdgeInsets.only(top: 10.0)),
+                      new TextFormField(
+                          decoration: new InputDecoration(
+                            labelText: "Date Absent",
+                            fillColor: Colors.white,
+                            border: new OutlineInputBorder(
+                              borderRadius: new BorderRadius.circular(25.0),
+                              borderSide: new BorderSide(
+                              ),
+                            ),
+                          ),
+                        validator: (val) {
+                          if(val.length==0) {return "Date cannot be empty";
+                          }else{return null;}
+                        },
+                        keyboardType: TextInputType.datetime,
+                        style: new TextStyle(
+                          fontFamily: "Poppins",
+                        ),
+                      ),
+                ],
+              ),
+            ),
+          ),
+        ),
     );
   }
 
@@ -21,4 +56,6 @@ class DetailAbsentActivity extends StatelessWidget {
       body: _initDetail(),
     );
   }
+
+  Color hexToColor(String code) {return new Color(int.parse(code.substring(1, 7), radix: 16) + 0xFF000000);}
 }
