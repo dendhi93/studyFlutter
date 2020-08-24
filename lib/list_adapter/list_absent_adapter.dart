2cb5e2072cdc5eecd1f4b678f6e197953094c557
dendhi93 <dendhi.fendi@gmail.com>
2020-08-24 08:54:52 +0700

    change statelesswidget into statefull widget in list home adapter, set State reason loading

--- lib/list_adapter/list_absent_adapter.dart
+++ lib/list_adapter/list_absent_adapter.dart
@@ -3,25 +3,43 @@ import 'package:absent_hris/model/ModelAbsensi.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class ListAdapter extends StatelessWidget{
+class ListAdapter extends StatefulWidget{
   final ModelAbsensi modelAbsensi;
   ListAdapter({this.modelAbsensi});
 
   @override
+  _ListAdapterState createState() => _ListAdapterState();
+}
+
+
+
+class _ListAdapterState extends State<ListAdapter> {
+  String reasonAbsent = "";
+
+  @override
+  void initState() {
+    super.initState();
+      reasonAbsent = widget.modelAbsensi.reason;
+      if(reasonAbsent != "") {
+        reasonAbsent = "\nReason " +widget.modelAbsensi.reason;
+      }else{return null;}
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Card(
-      child : Padding(
-        padding: const EdgeInsets.all(5.0),
-        child: Column(
-          children: <Widget>[
-            ListTile(
-              title: Text("Time " +modelAbsensi.timeIn + " - " +modelAbsensi.timeOut),
-              subtitle: Text(modelAbsensi.addressAbsent+"\nReason " +modelAbsensi.reason),
-              trailing: Text(modelAbsensi.dateAbsent),
-            )
-          ],
-        ),
-      )
+        child : Padding(
+          padding: const EdgeInsets.all(5.0),
+          child: Column(
+            children: <Widget>[
+              ListTile(
+                title: Text("Time " +widget.modelAbsensi.timeIn + " - " +widget.modelAbsensi.timeOut),
+                subtitle: Text(widget.modelAbsensi.addressAbsent+""+reasonAbsent),
+                trailing: Text(widget.modelAbsensi.dateAbsent),
+              )
+            ],
+          ),
+        )
     );
   }
 }
