0607351b56bd5feb2452ca386f4ad63d2886ce48
dendhi93 <dendhi.fendi@gmail.com>
2020-09-09 09:14:31 +0700

    add input text on login

--- lib/activity/LoginActivity.dart
+++ lib/activity/LoginActivity.dart
@@ -8,63 +8,76 @@ class LoginActivity extends StatefulWidget {
 }
 
 class _LoginActivityState extends State<LoginActivity> {
+  TextEditingController etLoginUsername = new TextEditingController();
+  TextEditingController etLoginPass = new TextEditingController();
 
-  // Widget _initLogin(BuildContext context){
-  //   return Form(
-  //       child: new Container(
-  //         padding: const EdgeInsets.all(10.0),
-  //         color: Colors.white,
-  //         child: new Container(
-  //             child: new Center(
-  //                 child: new Column(
-  //                     children: <Widget>[
-  //                       new Padding(padding: EdgeInsets.only(top: 10.0)),
-  //                       new TextFormField(
-  //                         decoration: new InputDecoration(
-  //                           labelText: "Username",
-  //                           fillColor: Colors.white,
-  //                           border: new OutlineInputBorder(
-  //                             borderRadius: new BorderRadius.circular(15.0),
-  //                             borderSide: new BorderSide(
-  //                             ),
-  //                           ),
-  //                         ),
-  //                         validator: (val) {
-  //                           if(val.length==0) {return "Date cannot be empty";
-  //                           }else{return null;}
-  //                         },
-  //                         keyboardType: TextInputType.datetime,
-  //                         style: new TextStyle(
-  //                           fontFamily: "Poppins",
-  //                         ),
-  //                       ),
-  //                     ],
-  //                 ),
-  //             ),
-  //         ),
-  //       ),
-  //   );
-  // }
+  Widget _initLogin(BuildContext context){
+    return Form(
+      child: new Container(
+        padding: const EdgeInsets.all(10.0),
+        color: Colors.white,
+
+        child: new Container(
+          child: new Center(
+            child: new Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                  new Padding(padding: EdgeInsets.only(top: 0.0)),
+                  new TextFormField(
+                    controller: etLoginUsername,
+                    decoration: new InputDecoration(
+                      labelText: "Username",
+                      fillColor: Colors.white,
+                      border: new OutlineInputBorder(
+                        borderRadius: new BorderRadius.circular(15.0),
+                        borderSide: new BorderSide(
+                        ),
+                      ),
+                    ),
+                    validator: (val) {
+                      if(val.length==0) {return "Username cannot be empty";
+                      }else{return null;}
+                    },
+                    keyboardType: TextInputType.text,
+                    maxLength: 15,
+                    style: new TextStyle(
+                      fontFamily: "Poppins",
+                    ),
+                  ),
+
+                  new Padding(padding: EdgeInsets.only(top: 10.0)),
+                  new TextFormField(
+                    controller: etLoginPass,
+                    decoration: new InputDecoration(
+                      labelText: "Password",
+                      fillColor: Colors.white,
+                      border: new OutlineInputBorder(
+                        borderRadius: new BorderRadius.circular(15.0),
+                        borderSide: new BorderSide(
+                        ),
+                      ),
+                    ),
+                    validator: (val) {
+                      if(val.length==0) {return "password cannot be empty";
+                      }else{return null;}
+                    },
+                    obscureText: true,
+                    maxLength: 15,
+                    style: new TextStyle(
+                      fontFamily: "Poppins",
+                    ),
+                  ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return new SafeArea(
-        child: new Form(
-            child: new Container(
-              padding: const EdgeInsets.all(10.0),
-              color: Colors.white,
-              child: new Container(
-                  child: new Center(
-                      child: new Column(
-                          children: <Widget>[
-                            new Padding(padding: EdgeInsets.only(top: 10.0)),
-
-                          ],
-                      ),
-                  ),
-              ),
-            ),
-        )
+      return Scaffold(body: _initLogin(context),
     );
   }
 }
