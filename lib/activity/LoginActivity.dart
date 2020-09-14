31bacc2fb33934e238a1889b0925a8deba8233c4
dendhi93 <dendhi.fendi@gmail.com>
2020-09-14 09:08:59 +0700

    add login validation

--- lib/activity/LoginActivity.dart
+++ lib/activity/LoginActivity.dart
@@ -1,6 +1,7 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class LoginActivity extends StatefulWidget {
   @override
@@ -11,13 +12,14 @@ class _LoginActivityState extends State<LoginActivity> {
   TextEditingController etLoginUsername = new TextEditingController();
   TextEditingController etLoginPass = new TextEditingController();
   bool _obscureText = true;
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   Widget _initLogin(BuildContext context){
     return Form(
+      key: _formKey,
       child: new Container(
         padding: const EdgeInsets.all(10.0),
         color: Colors.white,
-
         child: new Container(
           child: new Center(
             child: new Column(
@@ -69,6 +71,51 @@ class _LoginActivityState extends State<LoginActivity> {
                     maxLength: 15,
                     style: new TextStyle(
                       fontFamily: "Poppins",
+                    ),
+                  ),
+                new Padding(padding: EdgeInsets.only(top: 50.0)),
+                  new FlatButton(
+                    color: Colors.blue,
+                    textColor: Colors.white,
+                    disabledColor: Colors.blueGrey,
+                    disabledTextColor: Colors.black,
+                    padding: EdgeInsets.only(left: 70, top:20, right: 70, bottom: 20),
+                    splashColor: Colors.blueAccent,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18.0),
+                        side: BorderSide(color: Colors.yellow)
+                    ),
+                    onPressed: () {
+                      if (_formKey.currentState.validate()){
+                        showDialog(
+                          context: context,
+                          builder: (context) {
+                            return AlertDialog(
+                              content: Text("Transaction Success"),
+                              actions: <Widget>[
+                                FlatButton(child: Text('OK'),
+                                  onPressed: (){
+                                    Fluttertoast.showToast(
+                                        msg: "OK Thnks",
+                                        toastLength: Toast.LENGTH_LONG,
+                                        gravity: ToastGravity.CENTER,
+                                        timeInSecForIosWeb: 1,
+                                        backgroundColor: Colors.blue,
+                                        textColor: Colors.white,
+                                        fontSize: 16.0
+                                    );
+                                    Navigator.of(context, rootNavigator: true).pop();
+                                  },
+                                ),
+                              ],
+                            );
+                          },
+                        );
+                      }
+                    },
+                    child: Text(
+                      "LOG IN",
+                      style: TextStyle(fontSize: 20.0),
                     ),
                   ),
               ],
