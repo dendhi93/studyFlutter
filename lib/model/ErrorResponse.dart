437f003a010e03430e26fd826ba2ab3d59503d5b
dendhi93 <dendhi.fendi@gmail.com>
2020-10-15 17:05:54 +0700

    add eror response model and add loading class

---
+++ lib/model/ErrorResponse.dart
@@ -0,0 +1,12 @@
+class ErrorResponse {
+  int code;
+  String message;
+
+  ErrorResponse({this.code, this.message});
+
+  ErrorResponse.fromJson(Map<String, dynamic> json) {
+    code = json['Code'];
+    message = json['message'];
+  }
+
+}
