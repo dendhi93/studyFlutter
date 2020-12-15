c0e0dbadc56aff89bec30494715c3d26a5d9dfc7
dendhi93 <dendhi.fendi@gmail.com>
2020-12-15 08:41:16 +0700

    add unattendance adapter

--- lib/adapter/list_unattendance_adapter.dart
+++ lib/adapter/list_unattendance_adapter.dart
@@ -11,6 +11,26 @@ class ListUnAttendanceAdapter extends StatefulWidget{
 }
 
 class ListUnAttendanceAdapterState extends State<ListUnAttendanceAdapter> {
+  int statusUnAttendance = 0;
+  String imageUnAttendance = "";
+
+  @override
+  void initState() {
+    statusUnAttendance = widget.responseDtlUnAttendance.statusId;
+    super.initState();
+    if(statusUnAttendance != null){
+      if(statusUnAttendance == 1){
+        imageUnAttendance = "ic_sand.png";
+      }else if(statusUnAttendance == 2){
+        imageUnAttendance = "ic_ok_24.png";
+      }else{
+        imageUnAttendance = "ic_cross.png";
+      }
+    }else{
+      statusUnAttendance = null;
+      imageUnAttendance = null;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,7 +39,15 @@ class ListUnAttendanceAdapterState extends State<ListUnAttendanceAdapter> {
           padding: const EdgeInsets.all(5.0),
           child: Column(
             children: <Widget>[
-
+              ListTile(
+                leading: new Image.asset(
+                  "assets/images/"+imageUnAttendance ,
+                  fit: BoxFit.cover,
+                  width: 24.0,
+                ),
+                title: Text(widget.responseDtlUnAttendance.descUnattendance.trim()),
+                subtitle: Text(widget.responseDtlUnAttendance.transDate),
+              )
             ],
           ),
         )
