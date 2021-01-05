4af5d3797fef4487ec8c9db36a459aef73a637d8
dendhi93 <dendhi.fendi@gmail.com>
2021-01-05 08:40:19 +0700

    fixing typo function

--- lib/util/ConstanstVar.dart
+++ lib/util/ConstanstVar.dart
@@ -1,6 +1,6 @@
 
 class ConstanstVar{
-  static String urlApi = "http://192.168.142.241/coreHris/";
+  static String urlApi = "http://192.168.43.177/coreHris/";
   static String noConnectionMessage = "Device not connected to network";
   static String noConnectionTitle = "No Network";
   static String blankStatement = "please fill in the blank";
@@ -13,6 +13,7 @@ class ConstanstVar{
   static int inProgressClaimStatus = 1;
   static int approvedClaimStatus = 2;
   static int rejectClaimStatus = 3;
+
   static int selectStartDate = 5;
   static int selectEndDate = 15;
 
