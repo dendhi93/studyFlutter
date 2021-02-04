8a39cdaf5e0acb91b221379656b824e7e67dc07f
dendhi93 <dendhi.fendi@gmail.com>
2021-02-04 08:38:28 +0700

    add object response claim

--- lib/model/MasterClaim/GetClaim/ResponseClaimDataModel.dart
+++ lib/model/MasterClaim/GetClaim/ResponseClaimDataModel.dart
@@ -9,6 +9,7 @@ class ResponseClaimDataModel{
   String fileClaim;
   String nameRequester;
   String lowerUserId;
+  int idTrans;
 
   ResponseClaimDataModel(
       {this.claimDesc,
@@ -20,7 +21,8 @@ class ResponseClaimDataModel{
         this.descClaim,
       this.fileClaim,
       this.nameRequester,
-      this.lowerUserId});
+      this.lowerUserId,
+      this.idTrans = 0});
 
   ResponseClaimDataModel.fromJson(Map<String, dynamic> json) {
     claimDesc = json['claim_desc'];
@@ -33,5 +35,6 @@ class ResponseClaimDataModel{
     fileClaim = json['file_claim'];
     nameRequester = json['name_requester'];
     lowerUserId = json['user_id'];
+    idTrans = json['id_trans'];
   }
 }
