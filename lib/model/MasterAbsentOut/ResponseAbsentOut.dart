b2f275546f28f5f674c7801fdbc9e9fa41032fad
dendhi93 <dendhi.fendi@gmail.com>
2021-04-06 10:26:24 +0700

    prepare validate late sign in

--- lib/model/MasterAbsentOut/ResponseAbsentOut.dart
+++ lib/model/MasterAbsentOut/ResponseAbsentOut.dart
@@ -1,11 +1,13 @@
 class ResponseAbsentOut {
   int code;
   String absentOut;
+  String absentIn;
 
-  ResponseAbsentOut({this.code, this.absentOut});
+  ResponseAbsentOut({this.code, this.absentOut, this.absentIn});
 
   ResponseAbsentOut.fromJson(Map<String, dynamic> json) {
     code = json['Code'];
     absentOut = json['absent_out'];
+    absentIn = json['absent_in'];
   }
 }
