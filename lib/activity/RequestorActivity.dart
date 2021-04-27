a700d7ce05d5c6f9d57b31797b744507585eb2c0
dendhi93 <dendhi.fendi@gmail.com>
2021-04-27 13:04:44 +0700

    UI requestor enhancement part 1

--- lib/activity/RequestorActivity.dart
+++ lib/activity/RequestorActivity.dart
@@ -29,25 +29,38 @@ class RequestorActivityState extends State<RequestorActivity> {
     }
   }
 
+  //view
   @override
   Widget build(BuildContext context) {
     return Container(
         child : new Column(
             children: <Widget>[
               new Padding(padding: EdgeInsets.only(top: 10.0)),
+              new Image.asset('assets/images/ic_user_128.png', width: 190, height: 120,),
+              new Padding(padding: EdgeInsets.only(top: 25.0)),
+              new Text("Test", style: TextStyle(fontSize: 18)),
+              new Padding(padding: EdgeInsets.only(top: 20.0)),
               new Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: <Widget>[
                     new Container(
                       width: 200,
                       height: 95,
-                      color: Colors.cyanAccent,
+                      decoration: new BoxDecoration(
+                          color: Colors.lightBlueAccent,
+                          borderRadius: new BorderRadius.only(
+                            topLeft: const Radius.circular(10.0),
+                            topRight: const Radius.circular(10.0),
+                            bottomLeft: const Radius.circular(10.0),
+                            bottomRight: const Radius.circular(10.0),
+                          )
+                      ),
                       child: new Column(
                         children:  <Widget>[
-                          new Padding(padding: EdgeInsets.only(top: 5.0)),
-                          new Text("Absent In", style: TextStyle(fontSize: 21)),
+                          new Padding(padding: EdgeInsets.only(top: 9.0)),
+                          new Text("Absent In", style: TextStyle(fontSize: 21, color: Colors.white)),
                           new Padding(padding: EdgeInsets.only(top: 25.0)),
-                          new Text(intentAbsentIn, style: TextStyle(fontSize: 18)),
+                          new Text(intentAbsentIn, style: TextStyle(fontSize: 18, color: Colors.white)),
                         ],
                       ),
                     ),
@@ -55,13 +68,21 @@ class RequestorActivityState extends State<RequestorActivity> {
                     new Container(
                       width: 200,
                       height: 95,
-                      color: Colors.cyanAccent,
+                      decoration: new BoxDecoration(
+                          color: Colors.lightBlueAccent,
+                          borderRadius: new BorderRadius.only(
+                            topLeft: const Radius.circular(10.0),
+                            topRight: const Radius.circular(10.0),
+                            bottomLeft: const Radius.circular(10.0),
+                            bottomRight: const Radius.circular(10.0),
+                          )
+                      ),
                       child: new Column(
                         children:  <Widget>[
-                          new Padding(padding: EdgeInsets.only(top: 5.0)),
-                          new Text("Absent Out", style: TextStyle(fontSize: 21)),
+                          new Padding(padding: EdgeInsets.only(top: 9.0)),
+                          new Text("Absent Out", style: TextStyle(fontSize: 21, color: Colors.white)),
                           new Padding(padding: EdgeInsets.only(top: 25.0)),
-                          new Text(intentAbsentOut, style: TextStyle(fontSize: 18)),
+                          new Text(intentAbsentOut, style: TextStyle(fontSize: 18, color: Colors.white)),
                         ],
                       ),
                     ),
